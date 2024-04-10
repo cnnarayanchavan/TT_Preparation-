6091aa2d01a9880e1f5ed3c75bdfd5b944401fe7
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-10 19:26:40 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,1 +1,14 @@
-
+public class Constructor {
+  public static void main(String[]args){
+    Human obj1 = new Humna();
+  }
+}
+class Human{
+  int Limbs;
+  int Eyes:
+  int Age;
+  String Gender;
+  Human(){
+    System.out.println("Constructor is Called")
+  }
+}
