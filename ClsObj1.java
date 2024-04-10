3216c04660cdb52ad689500a1dd722fbb8f0e6d0
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-10 18:43:11 +0530

    Update ClsObj1.java

--- ClsObj1.java
+++ ClsObj1.java
@@ -8,7 +8,7 @@ public class ClsObj1{
     s1.Marks = 8.7;
     s1.Gender = "Male";
     System.out.println("Hey there " +s1.Name + " You are " +s1.Age + " year old");
-    System.out.println(s1);
+    System.out.println(s1.Gender);
     
       
     
