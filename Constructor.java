aa5d5ceb8b1a8b0f66e83a9b218f495d505b91d5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-10 19:33:26 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,6 +1,8 @@
 public class Constructor {
   public static void main(String[]args){
     Human obj1 = new Human();
+    System.out.println(obj1);
+    
   }
 }
 class Human{
@@ -8,7 +10,18 @@ class Human{
   int Eyes;
   int Age;
   String Gender;
+
+  //1]
   Human(){
     System.out.println("Constructor is Called");   //THIS IS NON-PARAMETERIZED CONSTRUDTOR
   }   
+
+  //2]
+    Human(int Limbs, int Eyes, int Age, String Gender){
+      this.Limbs = 4;
+      this.Eyes = 2;
+      this.Age = 6;
+      this.Gender = "Male";
+    }
+  
 }
