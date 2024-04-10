a26d9529b6f926fecc43eeead52481193d3c0fdc
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-04-10 19:29:40 +0530

    Aezakmii

--- Constructor.java
+++ Constructor.java
@@ -1,14 +1,14 @@
 public class Constructor {
   public static void main(String[]args){
-    Human obj1 = new Humna();
+    Human obj1 = new Human();
   }
 }
 class Human{
   int Limbs;
-  int Eyes:
+  int Eyes;
   int Age;
   String Gender;
   Human(){
-    System.out.println("Constructor is Called")
-  }
+    System.out.println("Constructor is Called");   //THIS IS NON-PARAMETERIZED CONSTRUDTOR
+  }   
 }
