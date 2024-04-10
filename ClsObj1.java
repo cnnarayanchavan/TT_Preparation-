491c719a8aacc442bcda716f799d48e1c6bf6828
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-10 18:46:55 +0530

    Update ClsObj1.java

--- ClsObj1.java
+++ ClsObj1.java
@@ -5,7 +5,8 @@ public class ClsObj1{
     Student s1 = new Student();
     s1.Name = "Narayana";
     s1.Age = 21;
-    s1.Marks = 8.7;
+    //s1.Marks = 8.7;
+    s1.Marks = 8.17f
     s1.Gender = "Male";
     System.out.println("Hey there " +s1.Name + " You are " +s1.Age + " year old");
     System.out.println(s1.Gender);
@@ -17,7 +18,8 @@ public class ClsObj1{
 class Student {
   String Name;
   int Age;
-  double Marks;
+  //double Marks;
+  folat Marks;
   String Gender;
 }
 
