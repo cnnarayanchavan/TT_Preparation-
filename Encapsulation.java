314f2cf6245998a2578459c363e72a0c402fa87e
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-11 09:50:56 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,1 +1,24 @@
+public class Encapsulation{
+  public static void main(String[]args){
+    BobUser bob = new BobUser();
+    bob.Name = "Narayana";
+    bob.Branch = "BankofBaroda";
+    bob.Amount = 45678;
+    bob.accOne();
+      
+  }
+}
 
+
+class Bank{
+  public String Name;
+  public String Branch;
+  protected int Rank;
+  private int Amount;
+  
+}
+class BobUser1 extends Bank {
+  public void accOne(){
+    System.out.println("I have an account in BOB")
+  }
+}
